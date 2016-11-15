bf9d45a3bc8473b7146ac06a0255a84c75a108c1
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-15 11:10:54 +0200

    authentication strategy service

---
+++ services/auth-strategy.js
@@ -0,0 +1,17 @@
+var Strategy = require('passport-http-bearer').Strategy;
+
+var user = require('./user.js');
+
+module.exports = new Strategy(
+    function(token, cb) {
+        user.loginWithToken(token)
+            .then(function(user) {
+                if (user) {
+                    cb(null, user);
+                } else cb(null, false);
+            })
+
+        .catch(function(err) {
+            cb(err);
+        });
+    });
