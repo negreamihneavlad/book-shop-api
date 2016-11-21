37dcfb2d4837cb88abb3aaea8df976327ccf98eb
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-21 15:07:26 +0200

    password : update, forgot, reset included

--- server.js
+++ server.js
@@ -7,14 +7,12 @@ var passport = require('passport');
 var bookController = require('./controllers/book-controller.js');
 var userController = require('./controllers/user-controller.js');
 
-var user = require('./services/user.js');
-
 var authStrategy = require('./services/auth-strategy.js');
 
 var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 passport.use(authStrategy);
@@ -25,16 +23,25 @@ app.get('/books', bookController.list);
 
 app.get('/search/:term', bookController.search);
 
-app.post('/books', passport.authenticate('bearer', { session: false }), bookController.create);
+app.post('/books', passport.authenticate('bearer', {session: false}), bookController.create);
 
-app.put('/books/:bookId', passport.authenticate('bearer', { session: false }), bookController.update);
+app.put('/books/:bookId', passport.authenticate('bearer', {session: false}), bookController.update);
 
-app.delete('/books/:bookId', passport.authenticate('bearer', { session: false }), bookController.destroy);
+app.delete('/books/:bookId', passport.authenticate('bearer', {session: false}), bookController.destroy);
 
 app.post('/login', userController.login);
 
 app.post('/sign-up', userController.signUp);
 
-app.listen(3000, function() {
+app.put('/account/edit', passport.authenticate('bearer', {session: false}), userController.update);
+
+app.put('/account/edit/change-password', passport.authenticate('bearer', {session: false}), userController.updatePassword);
+
+app.put('/account/forgot-password', userController.forgotPassword);
+
+app.put('/account/reset-password', userController.resetPassword);
+
+
+app.listen(3000, function () {
     console.log('Book Shop API listening on port 3000!');
 });
