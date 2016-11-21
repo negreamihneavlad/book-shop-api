1ce60ae350d275182312bb0b868c3220135a782f
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-21 15:08:05 +0200

    forgot password reset password update password included

--- services/user.js
+++ services/user.js
@@ -1,7 +1,11 @@
 module.exports = {
     loginWithEmail: loginWithEmail,
     loginWithToken: loginWithToken,
-    signUp: signUp
+    signUp: signUp,
+    update: update,
+    updatePassword: updatePassword,
+    forgotPassword: forgotPassword,
+    resetPassword: resetPassword
 };
 
 //////////////////////////////
@@ -20,11 +24,11 @@ var connection = mysql.createConnection({
 
 connection.connect();
 /**
+ * Searches for user email and password
  *
  * @param user
  */
 function loginWithEmail(user) {
-
     return new Promise(function (resolve, reject) {
         connection.query("SELECT * FROM users WHERE (email='" + user.email + "' AND password='" + md5(user.password) + "')", function (err, rows, result) {
             if (err) {
@@ -36,14 +40,14 @@ function loginWithEmail(user) {
     });
 }
 /**
+ * Search for user authentication token
  *
  * @param token
  * @param cb
  */
 function loginWithToken(token, cb) {
-
     return new Promise(function (resolve, reject) {
-        connection.query("SELECT * FROM users WHERE token='" + token + "'", function (err, rows, result) {
+        connection.query("SELECT * FROM users WHERE authToken='" + token + "'", function (err, rows, result) {
             if (err) {
                 reject(err);
             } else {
@@ -53,12 +57,13 @@ function loginWithToken(token, cb) {
     });
 }
 /**
+ * Creates a new user
  *
  * @param user
  */
 function signUp(user) {
     user.password = md5(user.password);
-    user.token = randomstring.generate();
+    user.authToken = randomstring.generate();
     user.isAdmin = 0;
     return new Promise(function (resolve, reject) {
         connection.query('INSERT INTO users SET ?', user, function (err, result) {
@@ -69,5 +74,74 @@ function signUp(user) {
             }
         });
     });
+}
+/**
+ * Update user data
+ *
+ * @param userId
+ * @param userData
+ */
+function update(userId, userData) {
+    return new Promise(function (resolve, reject) {
+        connection.query("UPDATE users SET ? WHERE ?", [userData, {id: userId}], function (err, result) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(_.assign({}, userData, {id: userId}));
+            }
+        });
+    });
+}
+/**
+ * Update user password
+ *
+ * @param userData
+ * @param newUserData
+ */
+function updatePassword(userData, newUserData) {
+    return new Promise(function (resolve, reject) {
+        if (userData.password == md5(newUserData.oldPassword)) {
+            return connection.query("UPDATE users SET ? WHERE ?", [{password: md5(newUserData.password)}, {id: userData.id}], function (err, result) {
+                if (err) {
+                    reject(err);
+                } else {
+                    resolve(_.assign({}, userData, {id: userData.id}));
+                }
+            });
+        } else return reject(500);
 
+    });
+}
+/**
+ *Insert at user details forgot password token
+ *
+ * @param user
+ */
+function forgotPassword(user) {
+    user.forgotPasswordToken = randomstring.generate();
+    return new Promise(function (resolve, reject) {
+        connection.query("UPDATE users SET ? WHERE ?", [{forgotPasswordToken: user.forgotPasswordToken}, {email: user.email}], function (err, result) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(user);
+            }
+        });
+    });
+}
+/**
+ * Resets password where forgot password token is found
+ *
+ * @param user
+ */
+function resetPassword(user) {
+    return new Promise(function (resolve, reject) {
+        connection.query("UPDATE users SET ? WHERE ?", [{password: md5(user.password)}, {forgotPasswordToken: user.code}], function (err, result) {
+            if (err) {
+                reject(err);
+            } else {
+                resolve(user);
+            }
+        });
+    });
 }
