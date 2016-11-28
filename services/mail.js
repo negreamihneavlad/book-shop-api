f285aa52e97dbadab72d560463fdfe695bd3a017
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-28 14:13:05 +0200

    send email refactored

--- services/mail.js
+++ services/mail.js
@@ -1,26 +1,19 @@
-var nodemailer = require('nodemailer');
-
 module.exports = {
-    emailChangePassword: emailChangePassword
-}
-// create reusable transporter object using the default SMTP transport
+    send: send
+};
+
+////////////////////////
+
+var nodemailer = require('nodemailer');
+var password = require('../services/password.js');
+
 var transporter = nodemailer.createTransport('smtps://bookshopwebsite@gmail.com:bookshop1@smtp.gmail.com');
 /**
  * Send email to user with its token
  *
- * @param user
+ * @param passwordResetData
  */
-function emailChangePassword(user) {
-    var mailOptions;
-
-    mailOptions = {
-        from: '"Book Shop" <bookshopwebsite@gmail.com>',
-        to: user.email,
-        subject: 'Change password',
-        text: 'Reset password: http://localhost:8080/#/account/reset-password?email='+user.email +'&code=' + user.forgotPasswordToken
-
-    };
-
-     return transporter.sendMail(mailOptions);
+function send(passwordResetData){
+    return transporter.sendMail(password.emailPasswordResetLink(passwordResetData));
 }
 
