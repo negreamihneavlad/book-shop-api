74e45aa4732519f05c75af98b87e8506855ae61a
negreavlad95 <negrea.vlad@yahoo.com>
2016-11-28 14:12:41 +0200

    formatted

--- server.js
+++ server.js
@@ -1,48 +1,38 @@
 var express = require('express');
-
-var app = express();
-
 var passport = require('passport');
-
 var bookController = require('./controllers/book-controller.js');
 var userController = require('./controllers/user-controller.js');
-
 var authStrategy = require('./services/auth-strategy.js');
-
 var bodyParser = require('body-parser');
+var app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-
 passport.use(authStrategy);
 
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header('Access-Control-Allow-Methods', 'POST, GET, OPTIONS, PUT, DELETE');
+    next();
+});
+
+//////////Books
 app.get('/books/:bookId', bookController.findOne);
-
 app.get('/books', bookController.list);
-
-app.get('/search/:term', bookController.search);
-
-app.get('/search-filters', bookController.searchFilters);
-
+app.get('/search', bookController.search);
 app.post('/books', passport.authenticate('bearer', {session: false}), bookController.create);
-
 app.put('/books/:bookId', passport.authenticate('bearer', {session: false}), bookController.update);
-
 app.delete('/books/:bookId', passport.authenticate('bearer', {session: false}), bookController.destroy);
 
+//////////User
 app.post('/login', userController.login);
-
 app.post('/sign-up', userController.signUp);
-
 app.put('/account/edit', passport.authenticate('bearer', {session: false}), userController.update);
-
 app.put('/account/edit/change-password', passport.authenticate('bearer', {session: false}), userController.updatePassword);
-
 app.put('/account/forgot-password', userController.forgotPassword);
-
 app.put('/account/reset-password', userController.resetPassword);
-
 
 app.listen(3000, function () {
     console.log('Book Shop API listening on port 3000!');
